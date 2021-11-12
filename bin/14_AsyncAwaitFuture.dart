c8ec19e45ba336c538d7bae97c47c3dc65189731
subhanassiddik <subhanassiddik@gmail.com>
2021-11-12 23:52:28 +0800

    Update Async Await Future

--- bin/14_AsyncAwaitFuture.dart
+++ bin/14_AsyncAwaitFuture.dart
@@ -2,18 +2,23 @@
 //keyword future akan membuat sebuah method tidak lansung dieksekusi,tp kita dapat mementukan kapan method tsb akan dieksekusi
 //keyword future tidak lansung me-return data
 
+// void main(List<String> args) async {
 void main(List<String> args) {
   Person p = Person();
 
   print('job 1');
   print('job 2');
+
   // p.getData();
+  // await p.getDataAsync();
+  // print('job 3 : ' + p.name);
+
   p.getDataAsync().then((_) => print('job 3 : ' + p.name));
   print('job 4');
 }
 
 class Person {
-  String name;
+  String name = 'default name';
 
   void getData() {
     name = 'joko';
